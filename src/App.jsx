b178fa34466fda6dd331f227251a7adf15c7caec
kamalrajms <kamalrajindtn@gmail.com>
2025-09-09 16:31:54 +0530

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,16 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useContext, createContext } from "react";
+import First from "./component/First";
+
+export const Pass = createContext();
 
 export default function App() {
-  //ex1
-  const inputRef = useRef();
-  const handlefocus = () => {
-    console.log("hello");
-  };
-  //eg2
-  const [sec, setSec] = useState(0);
-  const inputTimer = useRef();
-
-  useEffect(() => {
-    inputTimer.current = setInterval(() => {
-      setSec((prev) => prev + 1);
-    }, 1000);
-    return () => clearInterval(inputTimer.current);
-  }, []);
-
-  //ex3
-  const [newProductImg, setnewProductImg] = useState(true);
-  const [imageUrl, setImageUrl] = useState("");
-  const inputImg = useRef();
-
-  const handleImgChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const preview = URL.createObjectURL(file);
-      setImageUrl(preview);
-      setnewProductImg(false);
-    }
-  };
+  const name = "cat";
   return (
     <div>
-      {/* //eg1 */}
-      <input type="text" placeholder="Enter input" ref={inputRef} />
-      <button onClick={handlefocus}>click me</button>
-      {/* //eg2 */}
-      <h2>second :{sec}</h2>
-      <button onClick={() => clearInterval(inputTimer.current)}>Stop</button>
-      {/* //eg3 */}
-      {newProductImg ? (
-        <div className="image-box" onClick={() => inputImg.current.click()}>
-          upload image...
-        </div>
-      ) : (
-        <img
-          src={imageUrl}
-          alt="image"
-          onClick={() => inputImg.current.click()}
-        />
-      )}
-      <input
-        type="file"
-        accept="image/*"
-        ref={inputImg}
-        onClick={handleImgChange}
-        hidden
-      />
-      {/* <input type="range" />
-      <input type="time" />
-      <input type="color" /> */}
+      Main component
+      <Pass.Provider value={name}>
+        <First />
+      </Pass.Provider>
     </div>
   );
 }
---
+++ src/component/First.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Second from "./Second";
+
+export default function First() {
+  return (
+    <div>
+      First component
+      <Second />
+    </div>
+  );
+}
